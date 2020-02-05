9be6ef41a7441de94764c0ce8c2a62d74ad3bbcd
unknown <kevin.garzon@labinfo.is.escuelaing.edu.co>
2020-02-05 09:01:21 -0500

    lib2

--- lib2.py
+++ lib2.py
@@ -2,20 +2,31 @@ import lib
 
 def productoMatriz(A,B):
     M = []
-    for i in range(len(A)):
-        M.append([])
-        for j in range(len(B[0])):
-            M[i].append((0,0))
-            for k in range(len(B)):
-                M[i][j] = lib.suma(M[i][j],lib.producto(A[i][k], B[k][j]))
-    return(M)
+    if len(A[0]) == len(B):
+        for i in range(len(A)):
+            M.append([])        
+            for j in range(len(B[0])):
+                M[i].append((0,0))
+                for k in range(len(B)):
+                    M[i][j] = lib.suma(M[i][j], lib.producto(A[i][k], B[k][j]))
+                    
+        return M
+    else:
+        return 'Dimensiones incorrectas'
+
 
 def sumaMatriz(A,B):
     M = []
-    for i in range(len(A)):
-        M.append([])
-        for j in range(len(A[0])):
-            M[i].append(lib.suma(A[i][j], B[i][j]))
+    if len(A) == len(B) and len(A[0]) == len(B[0]):
+        for i in range(len(A)):
+            M.append([])        
+            for j in range(len(A[0])):
+                M[i].append(lib.suma(A[i][j], B[i][j]))
+        return M
+
+    else:
+        return 'Dimensiones incorrectas'
+
 
 def escalarMatriz(A,k):
     for i in range(len(A)):
@@ -23,6 +34,10 @@ def escalarMatriz(A,k):
             A[i][j] = lib.producto(k,A[i][j])
     return A
             
+def inversoAditivoMatriz(A):
+    A = escalarMatriz(A,-1)
+    return A
+
 def transpuestaMatriz(A):
     M = []
     for i in range(len(A[0])):
@@ -39,14 +54,19 @@ def conjugadaMatriz(A):
 
 def dagaMatriz(A):
     return transpuestaMatriz(conjugadaMatriz(A))
-    
+
 def main():
-    A = [[(3,4),(-1,0),(1,1)],
-         [(1,0),(0,2),(2,2)]
-        ]
-
-    B = [[(1,-1),(0,0)],
-         [(3,4),(-1,0)]
-        ]
-    print(productoMatriz(A,dagaMatriz(B)))
-
+    
+    A = [[(1,0),(1,0),(1,0)],
+         [(1,0),(1,0),(1,0)]]
+        
+    B = [[(1,0),(1,0),(1,0)],
+         [(1,0),(1,0),(1,0)],
+         [(1,0),(1,0),(1,0)]]
+    
+    C = transpuestaMatriz(A)
+    
+    for i in range(len(C)):
+        print(C[i])
+    
+main()
